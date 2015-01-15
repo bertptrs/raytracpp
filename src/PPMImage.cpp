2d4821636c205c95018d7bfb4d146ba9c4e538a5
Bert Peters <bert.ljpeters@gmail.com>
2015-01-15 20:43:33 +0100

    Bugfix: PPM rendering works again.

--- src/PPMImage.cpp
+++ src/PPMImage.cpp
@@ -11,6 +11,14 @@ void PPMImage::write(ostream& output) const {
 		<< width << " " << height
 		<< "\n" << 255 << "\n";
 	// Write the PPM bitmap
+	for (unsigned int y = 0; y < height; y++) {
+		for (unsigned int x = 0; x < width; x++) {
+			const unsigned char* pixel = getPixelArray(x, y);
+			for (int i = 0; i < CHANNELS; i++) {
+				output << pixel[i];
+			}
+		}
+	}
 	output.write((char*) &bitmap, width * height * CHANNELS);
 }
 
